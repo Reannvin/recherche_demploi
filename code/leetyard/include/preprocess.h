a13457056279ba0cffe3fe257f0963a6da569d03
Reannvin <582720301@qq.com>
2024-04-26 16:36:57 +0800

    fix

--- code/leetyard/include/preprocess.h
+++ code/leetyard/include/preprocess.h
@@ -13,3 +13,11 @@ struct ListNode
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
--- code/leetyard/temp/swin-dows.cpp
+++ code/leetyard/temp/swin-dows.cpp
@@ -14,3 +14,20 @@ void slidingWindow(string s, string t){
         }
     }
 }
+
+TreeNode* find(TreeNode* root, int val1, int val2){
+    if(root == nullptr){return nullptr;}
+    if(root->val == val1 || root->val ==val2){
+        return root;
+    }
+    TreeNode* left = find(root->left,val1,val2);
+    TreeNode* right = find(root->right,val1,val2);
+    if(left != nullptr && right != nullptr){
+        return root;
+    }
+    return left != nullptr ? left : right;
+}
+
+TreeNode* lowestCommonAncestor(TreeNode* root,TreeNode* p, TreeNode* q){
+    return find(root,p->val,q->val);
+}
